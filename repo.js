4db06a1426f42a14ecf13e899a91e05352fe9857
Dawid_Wendt <d.wendt@op.pl>
2025-03-28 07:14:38 +0100

    1.4.5

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "@Wensomt",
+    "id": "Wensomt",
+    "patches": {
+        "ReworkPoFV": "Basicly Rework Touhou PoFV with Some Balance Changes",
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/Wensomt/"
+    ],
+    "title": "Wensomt's Repository"
+}
